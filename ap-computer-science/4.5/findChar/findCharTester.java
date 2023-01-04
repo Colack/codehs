4ca6d9919d09b41e3488485474b4a062863ce3cc
Jack <UniformKitten281@gmail.com>
2023-01-04 08:16:52 -0800

    Update findCharTester.java

--- ap-computer-science/4.5/findChar/findCharTester.java
+++ ap-computer-science/4.5/findChar/findCharTester.java
@@ -3,21 +3,19 @@ public class findCharTester
     public static void main(String[] args)
     {
         // Start here!
-        String word = "This is a sentence";
-        String key = "a";
-        System.out.println("The character "+ key+ " is in the String "+ word + ": " + findChar(word, key));
+        String word = "Sussy Imposter";
+        String key = "e";
+        System.out.println("The character "+ key+ " is in the String '"+ word + "': " + findChar(word, key));
     }
     
     public static boolean findChar(String string, String key)
     {
-        int index = 0;
         String character;
-        while (true) {
-            character = string.substring(index,index+1);
+        for (int i = 0; i < string.length(); i++) {
+            character = string.substring(i,i+1);
             if (character.equals(key)) {
                 return true;
             }
-            index++;
         }
         return false;
     }
