4b5679ec49bb4db2fd137fe1c131ca7704e437c2
Jack <UniformKitten281@gmail.com>
2023-01-05 07:57:04 -0800

    Create DogTester.java

---
+++ ap-computer-science/5.2/Dog/DogTester.java
@@ -0,0 +1,11 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        Dog dog1 = new Dog("Karel", "Retriever", true);
+        Dog dog2 = new Dog("Clover", "Corgi");
+        
+        System.out.println(dog1);
+        System.out.println(dog2);
+    }
+}
