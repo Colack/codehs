0c845a09e734598bd65d920e6f6095ecf19029ee
Jack <UniformKitten281@gmail.com>
2023-01-12 19:12:00 -0800

    Create CYOA.java

---
+++ ap-computer-science/5.3/CYOA/CYOA.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class CYOA
+{
+    public static void main(String[] args)
+    {
+        // Jojo part 2 the game
+        
+        // -- Explanation of story --
+        
+        // First choice: Story mode
+        
+            // Choices
+            
+                // Obtain red stone
+                
+                    // Choices
+                    
+                        // Go to speedwagon
+                        
+                        // Go to pillar man
+                        
+                        // Go to secret base
+                
+                // Fight pillar men
+                
+                    // Choices
+                    
+                        // Esidisi
+                        
+                        // Wamuu
+                        
+                        // Kars
+                
+                // Hang out with Caesar
+                
+                    // Choices
+                        
+                        // Play video games
+                        
+                        // Play board games
+                        
+                        // Record a movie
+        
+        // Second choice: Settings
+        
+            // Settings go here
+        
+        // Third choice: Exit
+        
+            // Exits game
+    }
+}
