4efe6f89e909b52bff0d47843229e659aa3e5db5
Jack <UniformKitten281@gmail.com>
2023-02-13 09:41:38 -0800

    Update and rename Variables125.java to Variables1-2-5.java

--- ap-computer-science/1.2/Variables/Variables1-2-5.java
+++ ap-computer-science/1.2/Variables/Variables1-2-5.java
@@ -2,7 +2,7 @@ public class Variables
 {
     public static void main(String[] args)
     {
-        int year = 2022;
+        int year = 2023;
         System.out.println("The current year is " + year);
     }
 }
