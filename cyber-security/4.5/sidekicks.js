812f405ef689a5be231843cec4c05b98c3a49b1a
Jack <UniformKitten281@gmail.com>
2023-10-13 08:04:00 -0700

    Create sidekicks.js

---
+++ cyber-security/4.5/sidekicks.js
@@ -0,0 +1,13 @@
+var sidekicks = {};
+    
+// Enter some sidekicks into the database
+sidekicks["Wonder Woman"] = "Wonder Girl";
+sidekicks["Mermaid Man"] = "Barnacle Boy";
+sidekicks["Finn the Human"] = "Jake the Dog";
+    
+// Add Batman's sidekick Robin
+    sidekicks["Batman"] = "Robin";
+// Get the name of Mermaid Man's sidekick and print it out
+println(sidekicks["MermaidMan"]);
+// Add your sidekick to the database
+sidekicks["Jack"] = "Jack 2";
