d4a1bb2fd2889bd3e09acd439bb38e19bfeaf555
Jack <UniformKitten281@gmail.com>
2023-10-13 08:01:36 -0700

    Create DailyActivities.js

---
+++ cyber-security/4.1/DailyActivities.js
@@ -0,0 +1,23 @@
+// Intialize your two variables here.
+var partOfDay = "morning";
+var time = "6";
+
+
+// This will print your first statement. Run code and check it out!
+println("In the " + partOfDay + " I wake up at " + time);
+	
+// Change the values for partOfDay (to afternoon) and the time.
+partOfDay = "afternoon";
+time = "12";
+
+
+// This should print lunchtime.
+println("In the " + partOfDay + " I eat lunch at " + time);
+	
+// Change the values for partOfDay (to evening) and the time.
+partOfDay = "evening";
+time = "11:30";
+
+	
+// This should print bedtime.
+println("In the " + partOfDay + " I go to sleep at " + time);
