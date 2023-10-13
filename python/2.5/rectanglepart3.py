487bfd416c8cce32cb6f462223673e2c1a931798
Jack <UniformKitten281@gmail.com>
2023-10-13 08:19:12 -0700

    Create rectanglepart3.py

---
+++ python/2.5/rectanglepart3.py
@@ -0,0 +1,7 @@
+length = input("What is the length?: ")
+width = input("What is the width?: ")
+length = int(length)
+width = int(width)
+
+print("Area: " + str(length * width))
+print("Perimeter: "+ str(2 * (length + width)))
