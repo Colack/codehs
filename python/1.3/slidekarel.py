0a40522e122f84cf16c7cc67b06f119b4d0ef5aa
Jack <UniformKitten281@gmail.com>
2023-10-17 08:31:58 -0700

    Create slidekarel.py

---
+++ python/1.3/slidekarel.py
@@ -0,0 +1,17 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+put_ball()
+turn_right
+move()
+
+turn_right()
+move()
+put_ball()
+
+move()
+turn_left()
+move()
+put_ball()
