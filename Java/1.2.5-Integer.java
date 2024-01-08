cbe25311393aaf984af75f29286992dded8d17fd
Jack Spencer <UniformKitten281@gmail.com>
2024-01-08 09:36:18 -0800

    Very important update :trollface:

--- Java/1.2.5-Integer.java
+++ Java/1.2.5-Integer.java
@@ -2,7 +2,7 @@ public class Variables
 {
     public static void main(String[] args)
     {
-        int year = 2023;
+        int year = 2024;
         System.out.println("The current year is " + year);
     }
 }
