549fb4e61618c33f7875846f749bfba32157e344
Jack Spencer <UniformKitten281@gmail.com>
2024-01-16 09:12:39 -0800

    Create CodeABio.py

---
+++ Python/Hour-Of-Code/CodeABio.py
@@ -0,0 +1,6 @@
+first_name = "Jack"
+last_name = "Spencer"
+city = ""
+state = ""
+
+print("Hello! My name is " + first_name + " " + last_name + ". I am from " + city + ", from the state of " + state + ".")
